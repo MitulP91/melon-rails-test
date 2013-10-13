ae1539a79421c8a81682c2f32970983e7876fc20
MitulP91 <mitulp91@gmail.com>
2013-10-13 15:10:20 -0400

    First Commit

---
+++ app/assets/javascripts/resources.js
@@ -0,0 +1,53 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	 // area01 tileset
+	{name: "area01_level_tiles",  type:"image", src: "/assets/map/area01_level_tiles.png"},
+
+	// meta tileset
+	{name: "metatiles32x32",  type:"image", src: "/assets/map/metatiles32x32.png"},
+    // the main player spritesheet
+    {name: "gripe_run_right",     type:"image", src: "/assets/sprite/gripe_run_right.png"},
+    // the parallax background
+    {name: "area01_bkg0",         type:"image", src: "/assets/area01_bkg0.png"},
+    {name: "area01_bkg1",         type:"image", src: "/assets/area01_bkg1.png"},
+    // Title Screen Background
+    {name: "title_screen",         type:"image", src: "/assets/gui/title_screen.png"},
+    // the spinning coin spritesheet
+    {name: "spinning_coin_gold",  type:"image", src: "/assets/sprite/spinning_coin_gold.png"},
+    // our enemty entity
+    {name: "wheelie_right",       type:"image", src: "/assets/sprite/wheelie_right.png"},
+    // game font
+    {name: "32x32_font",          type:"image", src: "/assets/font/32x32_font.png"},
+    // Bullet Sprite
+    {name: "bullet",       type:"image", src: "/assets/sprite/bullet.png"},
+	
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+ 	{name: "area01", type: "tmx", src: "/assets/map/area01.tmx"}
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+	// {name: "dst-inertexponent", type: "audio", src: "/assets/audios/dst-inertexponent.mp3", channel : 1},
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+	// {name: "cling", type: "audio", src: "/assets/audios/cling.mp3", channel : 2},
+ //    {name: "stomp", type: "audio", src: "/assets/audios/stomp.mp3", channel : 1},
+ //    {name: "jump",  type: "audio", src: "/assets/audios/jump.mp3", channel : 1}
+];
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MelonjsRails::Application.config.secret_token = '44b7b4d539151cf7f3a80b4b8f0b6425950df5dc02b3a51b111e96640b0984a0acf6d22943de3fd782c8eb59aad2b71403c0db91107fb1bc8d16e01d15f89f78'
